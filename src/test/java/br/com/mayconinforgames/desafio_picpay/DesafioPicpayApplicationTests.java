4f7b3cb39fa1a37095ba7bc504a9181f8a887aaa
maycon-santana <115080830+maycon-santana@users.noreply.github.com>
2024-07-06 20:17:37 -0300

    Criando o projeto

---
+++ src/test/java/br/com/mayconinforgames/desafio_picpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.desafio_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
